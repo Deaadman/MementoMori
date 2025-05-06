533a0fcb659a042c73caf24c4573741fce5460eb
Deadman <47807486+Deaadman@users.noreply.github.com>
2025-05-06 16:56:19 +0930

    Nevermind...

--- src/main/java/com/deadman/momento_mori/mixin/ItemsMixin.java
+++ src/main/java/com/deadman/momento_mori/mixin/ItemsMixin.java
@@ -1,3 +1,4 @@
+/*
 package com.deadman.momento_mori.mixin;
 
 import net.minecraft.component.DataComponentTypes;
@@ -5,6 +6,7 @@ import net.minecraft.component.type.DeathProtectionComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.Rarity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -15,7 +17,8 @@ public class ItemsMixin {
 
     // Change the rarity of the Totem of Undying to EPIC instead of UNCOMMON.
     @Shadow
+    @Final
     public static final Item TOTEM_OF_UNDYING = register(
             "totem_of_undying",
             new Item.Settings().maxCount(1).rarity(Rarity.EPIC).component(DataComponentTypes.DEATH_PROTECTION, DeathProtectionComponent.TOTEM_OF_UNDYING));
-}
+}*/
